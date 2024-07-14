05d0819556158c4087a396e21626f09639f9ebae
guildmember145 <sr.achucarrohector@hotmail.com>
2024-07-14 10:58:11 -0300

    get_route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+const app = new Elysia().get("/", () => "Build Elysia RESTFUL API")
+.get('/post/:id',({params: {id}}) =>{return {
+  id:id,
+   title: "Bun Restful API",
+  }})
+.listen(3000);
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
