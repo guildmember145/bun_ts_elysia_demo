16eed2c66df2fab6b62a577eac5817d680e2b387
guildmember145 <sr.achucarrohector@hotmail.com>
2024-07-14 11:26:18 -0300

    new_routes

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Build Elysia RESTFUL API")
 .get('/post/:id',({params: {id}}) =>{return {
-  id:id,
-   title: "Bun Restful API",
-  }})
+   id:id,
+   title: "Bun Restful API"
+  }}).post('/post',(body)=>{return body})
+.get('/track/*',() => {return 'Track Route'})
 .listen(3000);
 
 
